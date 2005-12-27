de7829d6330e2c86b8c6b9f26f5f4a4977eab1b4
mcnicks <mcnicks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-12-27 20:47:59 +0000

    Added REST and name/value caching that used to reside within the flickr-post plugin.
    git-svn-id: https://plugins.svn.wordpress.org/url-cache/trunk@4280 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- url-cache.php
+++ url-cache.php
@@ -107,4 +107,131 @@ function url_cache ( $remote_url = "" ) {
     return $remote_url;
 }
   
+
+
+/* 
+ * uc_get_rest_response
+ *  - $method is the REST method to be cached
+ *  - $slug is used to make the cached response more unique
+ *  - (optional) $timeout is a specific timeout value to use
+ *  - (optional) $any will return any available cached
+ *    response, regardless of how stale it is.
+ *
+ * This function uses $method and $cache to make up a unique filename
+ * which is used to store REST responses. If the file associated with
+ * $request and $slug exists, its contents are returned. If the file
+ * is stale, then nothing is returned, which should prompt a refresh.
+ */
+
+function uc_get_rest_response( $method, $slug, $timeout = 0, $any = 0 ) {
+
+  // Return if the method and slug are not specified.
+
+  if ( ! $method || ! $slug ) return;
+
+  // Set the timeout to the default value if none has been specified.
+
+  if ( ! $timeout ) $timeout = UC_CACHE_TIMEOUT;
+
+  // Work out the file name that the response should be cached in.
+
+  $filename = UC_CACHE_DIR."rest--$method--$slug";
+
+  // Return if the file does not exist.
+
+  if ( ! @file_exists( $filename ) ) return;
+
+  // Check whether the cached response is stale, unless we have been
+  // told to return anything that is available.
+
+  if ( ! $any )
+    if ( ( @filemtime( $filename ) + $timeout ) < ( time() ) )
+      return;
+
+  // Otherwise, open it and return the contents.
+
+  $handle = @fopen( $filename, "r" );
+
+  if ( $handle ) {
+
+    $cached_response = "";
+
+    while ( $part = @fread( $handle, 8192 ) ) {
+      $cached_response .= $part;
+    }
+
+    @fclose( $handle );
+
+    return $cached_response;
+  }
+}
+
+
+
+/*
+ * uc_cache_rest_response
+ *  - $method is the REST method to be cached
+ *  - $slug is used to make the cached response more unique
+ *  - $response is the actual response that we should cache
+ *
+ * This function uses $method and $cache to make up a unique filename
+ * which is used to store REST responses. When called, the function
+ * writes the given response to that filename.
+ */
+
+function uc_cache_rest_response( $method, $slug, $response ) {
+
+  // Return if the arguments are not specified.
+
+  if ( ! $method || ! $slug  || ! $response ) return;
+
+  // Work out the file name that the response should be cached in.
+
+  $filename = UC_CACHE_DIR."rest--$method--$slug";
+
+  // Open it for writing and dump the response in.
+
+  $handle = @fopen( $filename, "w+" );
+
+  if ( $handle ) {
+
+    @fwrite( $handle, $response );
+    @fclose( $handle );
+  }
+
+  // That almost seemed too simple.
+}
+
+
+
+/*
+ * uc_cache_value
+ *  - $name the name of the value to cache
+ *  - $value the value itself
+ *
+ * This function uses the standard caching functions above to cache
+ * name/value pairs. At the moment this is a bit of a fudge, making
+ * use of the special method, "value". */
+
+function uc_cache_value( $name, $value ) {
+  return uc_cache_rest_response( "value", $name, $value );
+}
+
+
+
+/*
+ * uc_get_cached_value
+ *  - $name the name of the value to cache
+ *  - returns the cached value associated with $name
+ *
+ * This function uses the standard caching functions above to return
+ * a previously cached value associated with a name/value pair. At the
+ * moment this is a bit of a fudge, making use of the special method,
+ * "value".
+ */
+
+function uc_get_value( $name, $timeout = 0 ) {
+  return uc_get_rest_response( "value", $name, $timeout );
+}
+
 ?>
